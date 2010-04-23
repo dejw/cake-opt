ab94467bf256ee5575dd5d96c34ce75acac7ece7
Dawid Fatyga <dawid.fatyga@gmail.com>
2010-04-24 00:51:11 +0200

    Parameters are parsed now.

--- src/Instruction/LinkTo.php
+++ src/Instruction/LinkTo.php
@@ -14,8 +14,7 @@ class Opc_Instruction_LinkTo extends Opt_Compiler_Processor {
     
     /*
      * Renders the anchor
-     * 
-     * TODO: parse parametres
+     * TODO: fix parameters escaping
      */
     public function processNode(Opt_Xml_Node $node){
         /* Parse attributes */
@@ -24,42 +23,74 @@ class Opc_Instruction_LinkTo extends Opt_Compiler_Processor {
             'action'     => array(self::REQUIRED, self::HARD_STRING)
         );
         $optional = array(
-            'params'     => array(self::OPTIONAL, self::HARD_STRING, ''),
             'anchor'     => array(self::OPTIONAL, self::HARD_STRING, null),
-            'full'       => array(self::OPTIONAL, self::BOOL, false)
+            'full'       => array(self::OPTIONAL, self::BOOL, false),
+            '__UNKNOWN__' => array(self::OPTIONAL, self::EXPRESSION, null)
         );
         $this->_extractAttributes($node, $url);
-        $this->_extractAttributes($node, $optional);
+        $unknown = $this->_extractAttributes($node, $optional);
 
+        $params = $this->_extractParams($unknown);
+        
         /* ... and remove them */
 		$node->removeAttribute('controller');
         $node->removeAttribute('action');
-        $node->removeAttribute('params');
         $node->removeAttribute('anchor');
         $node->removeAttribute('full');
 
-        /* Generate the url */
-        $url = h(Router::url($url, $optional['full']));
-			
-        $node->set('call:attribute-friendly', true);
-        $node->set('nophp', true);
-        $node->set('noEntitize', true);
+        foreach($unknown as $param => $value)
+            $node->removeAttribute($param);
 
-        $node->addBefore(Opt_Xml_Buffer::TAG_NAME, "a");
-        $node->addAttribute(new Opt_Xml_Attribute('href', $url));
+        $node->set('call:attribute-friendly', true);
+        $node->set('noEntitize', true);
 
         /* Add anchor when it is available */
         if($node->get('single') and is_null($optional['anchor']))
             throw new Opt_AttributeNotDefined_Exception('anchor', 'single opt:link_to tag');
-        
+
         $node->appendChild(new Opt_Xml_Text($optional['anchor']));
 
+        /* Output anchor now If we have only controller and action attributes */
+        if(count($params) == 0){
+            /* Generate the url */
+            $url = h(Router::url(array_merge($url, $params), $optional['full']));
+            $node->set('nophp', true);
+            
+            $node->addBefore(Opt_Xml_Buffer::TAG_NAME, "a");
+            $node->addAttribute(new Opt_Xml_Attribute('href', $url));
+        } else {
+            $node->set('dynamic', true);
+            /* otherwise emit the url generator */
+            $href = "<?php echo h(Router::url(array(" .
+                    'controller => "' . $url['controller']. '",' .
+                    'action => "' . $url['action']. '",';
+            foreach($params as $param)
+                $href .= $param . ",";
+
+            $href .= "), " . ($optional['full'] ? "true" : "false") . ")); ?>";
+            
+            $node->addBefore(Opt_Xml_Buffer::TAG_NAME, "echo 'a';");
+            $attr = new Opt_Xml_Attribute('href', $href);
+            $node->addAttribute($attr);
+        }
         $node->set('postprocess', true);
+        $this->_compiler->set('escape', false);
         $this->_process($node);
     }
 
     public function postprocessNode(Opt_Xml_Node $node){
         $node->setNamespace(null);
+    }
+
+    /* Searches for parameters named param* = value */
+    private function _extractParams($params){
+        $return = array();
+        foreach($params as $name => $value){
+            if(preg_match("/^param(.*)/", $name))
+                $return[$name] = $value;
+        }
+        ksort($return);
+        return array_values($return);
     }
 }
 
